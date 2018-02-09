0476a9b8d2264bccd709c534976be52faaf77906
Robbyro89 <gallery.laroche.id@gmail.com>
2018-02-10 02:57:17 +0800

    Create home page and session

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
 
   has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
   has_and_belongs_to_many :attended_events, class_name: 'Event'
-end
 
-before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
